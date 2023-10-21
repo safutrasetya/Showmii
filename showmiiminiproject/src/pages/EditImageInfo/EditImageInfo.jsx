6fad6f26c5a92d56fa6baf212ca28f7e59c82f2b
safutrasetya <safutrasetya@gmail.com>
2023-10-21 14:56:18 +0700

    updt edit image info

--- showmiiminiproject/src/pages/EditImageInfo/EditImageInfo.jsx
+++ showmiiminiproject/src/pages/EditImageInfo/EditImageInfo.jsx
@@ -3,9 +3,42 @@ import Navbar from "../../components/Navbar/Navbar";
 import "../../essentialcss/essentialcss.css"
 import "./EditImageInfo.css"
 import TESTIMAGES from "../../testimage/TestImages";
+import { useState } from "react";
 
 
 export default function EditImageInfo(){
+
+    const [formInput, setFormInput] = useState({
+        imgname: "" ,
+        imgdesc : ""
+    })
+    const [error, setError] = useState("")
+    function handleImgName(e){
+        setFormInput({...formInput, imgname : e.target.value})
+        console.log(formInput)
+    }
+
+    function handleImgDesc(e){
+        setFormInput({...formInput, imgdesc : e.target.value})
+        console.log(formInput)
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        const pattern = /[{}+=><`;\:]/
+        // validasi
+        if(formInput.imgname == ""){
+            setError("Image name can not be empty")
+        }else if(pattern.test(formInput.imgname)){
+            setError("Image name must not contain {}+=><`;\\: characters!")
+        }else{ // if all valid
+            console.log("Data accepted ", formInput)
+                 
+        
+        }
+
+
+    }
 
     return(
         <>
@@ -21,14 +54,22 @@ export default function EditImageInfo(){
                             </div>
                             <div className="sub-maindiv-infos">
                                 <div className="form-container">
-                                    <form>
+                                    <form onSubmit={handleSubmit}>
                                         <div className="form-part">
                                             <label className="input-label fonts32 fontw500" htmlFor="inputimgname">Image Name : </label>
-                                            <input className="input-imagename" name="inputimgname" type="text"/>
+                                            <input onChange={handleImgName} className="input-imagename" name="inputimgname" type="text"/>
                                         </div>
                                         <div className="form-part">
                                             <label className="input-label fonts24 fontw700" htmlFor="inputimgname">Image Description :</label>
-                                            <textarea className="input-imagedesc" name="inputimgname" type="text"></textarea>
+                                            <textarea onChange={handleImgDesc} className="input-imagedesc" name="inputimgname" type="text"></textarea>
+                                        </div>
+                                        <div className="form-part">
+                                            <div className="alert">
+                                                <div class="alert alert-danger" role="alert">
+                                                    {error}
+                                                </div>
+                                                <p>{error}</p>
+                                            </div>
                                         </div>
                                         <div className="form-part-final">
                                             <button className="btn-classic cancel-btn font-roboto fonts20 fontw700" type="button">Cancel</button>
